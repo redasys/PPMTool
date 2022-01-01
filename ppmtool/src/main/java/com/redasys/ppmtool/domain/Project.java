5350331be067c16607ab2ce1144fd171044f9093
redasys <bob.egan.dev@gmail.com>
2022-01-01 17:43:30 -0500

    Attribute validation

--- ppmtool/src/main/java/com/redasys/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/redasys/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package com.redasys.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -9,13 +13,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message="Project Name is required")
     private String projectName;
+    @NotBlank(message="Project Identifier is required")
+    @Size(min=4, max=5, message="Please enter 4 or 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message="Project description is required")
     private String description;
+    @JsonFormat(pattern="yyyy-mm-dd")
     private Date startDate;
+    @JsonFormat(pattern="yyyy-mm-dd")
     private Date endDate;
 
+    @JsonFormat(pattern="yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern="yyyy-mm-dd")
     private Date updated_At;
 
     public Long getId() {
