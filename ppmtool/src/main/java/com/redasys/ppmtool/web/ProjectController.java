80707e0a8087c562e642a081ae7106dafe57bb03
redasys <bob.egan.dev@gmail.com>
2022-01-01 17:13:03 -0500

    Persist Project Entity

---
+++ ppmtool/src/main/java/com/redasys/ppmtool/web/ProjectController.java
@@ -0,0 +1,24 @@
+package com.redasys.ppmtool.web;
+
+import com.redasys.ppmtool.domain.Project;
+import com.redasys.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/project")
+public class ProjectController {
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("")
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+        Project project1 = projectService.saveOrUpdate(project);
+        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+    }
+}
